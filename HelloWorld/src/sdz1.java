934b7983d07e640ec7a76ce2fed3c92d85616d2b
Fedorya <seb_diouf@hotmail.com>
2013-07-30 16:10:04 +0200

    Conditions

--- HelloWorld/src/sdz1.java
+++ HelloWorld/src/sdz1.java
@@ -4,9 +4,23 @@ import java.util.Scanner;
 public class sdz1 {
 
 	public static void main(String[] args) {
-
-		//test
-		//Pewds Biffle Marzia
+		
+		int i = -1;
+		if (i < 0){
+			System.out.print("Le résultat est négatif");
+		}
+		
+		else if (i == 0){
+			
+			System.out.print("Le résultat est de zéro");
+		}
+		
+		else{
+			System.out.print("Le résultat est positif");
+			
+		}
+		
+		}
 	}
 
-}
+
