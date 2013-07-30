99fd9c8a60ec062b51abb7e10e14deb545ab632e
Fedorya <seb_diouf@hotmail.com>
2013-07-30 16:12:57 +0200

    Conditions Mix Scanner

--- HelloWorld/src/sdz1.java
+++ HelloWorld/src/sdz1.java
@@ -5,7 +5,9 @@ public class sdz1 {
 
 	public static void main(String[] args) {
 		
-		int i = -1;
+		System.out.print("Entrez un chiffre positif ou négatif.");
+		Scanner sc = new Scanner (System.in);
+		int i = sc.nextInt();
 		if (i < 0){
 			System.out.print("Le résultat est négatif");
 		}
