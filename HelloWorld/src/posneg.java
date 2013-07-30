45f945b5dba68b4b47d3b6f9c04d912a9cd019ad
Fedorya <seb_diouf@hotmail.com>
2013-07-30 16:23:49 +0200

    Ajout du code positif-négatif.

--- HelloWorld/src/posneg.java
+++ HelloWorld/src/posneg.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 
 
-public class sdz1 {
+public class posneg {
 
 	public static void main(String[] args) {
 		
 		//Mix entre le scanner et les conditions.
+		//Revision 0.2
 		
-		System.out.print("Entrez un chiffre positif ou négatif.");
+		System.out.println("Entrez un chiffre positif ou négatif.");
 		Scanner sc = new Scanner (System.in);
 		int i = sc.nextInt();
 		if (i < 0){
