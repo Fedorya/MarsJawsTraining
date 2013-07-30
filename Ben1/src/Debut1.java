915935a55e4f74b77fc9c9c1391a12d4402fcdd1
WiwiPoupsi <veritas75100@gmail.com>
2013-07-30 16:17:52 +0200

    rev 0.1

--- Ben1/src/Debut1.java
+++ Ben1/src/Debut1.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
+
 
 public class Debut1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //Chalut comment ça va ?
+		Scanner sc = new Scanner (System.in);
+		System.out.println("Veuillez saisir un mot : ");
+		String str = sc.nextLine();
+		System.out.println("Vous avez écris : " + str);
+		
 	}
 
 }
