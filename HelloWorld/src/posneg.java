3cae059585e2c1394058391700e3d99f29d3ddb8
Fedorya <seb_diouf@hotmail.com>
2013-07-30 16:43:54 +0200

    Revision 0.3 du Posneg

--- HelloWorld/src/posneg.java
+++ HelloWorld/src/posneg.java
@@ -5,8 +5,11 @@ public class posneg {
 
 	public static void main(String[] args) {
 		
+		//Revision 0.2 :
 		//Mix entre le scanner et les conditions.
-		//Revision 0.2
+		
+		//Revision 0.3 :
+		//Ajout du localisateur numérique.
 		
 		System.out.println("Entrez un chiffre positif ou négatif.");
 		Scanner sc = new Scanner (System.in);
@@ -23,6 +26,23 @@ public class posneg {
 		else{
 			System.out.print("Le résultat est positif");
 			
+		}
+		
+		int j = i;
+		if (j >= 0 && j <= 100){
+			System.out.print(" ainsi que compris entre 0 et 100");
+		}
+		else if (j >= 100){
+			System.out.print(" ainsi que supérieur a 100");
+			
+		}
+		
+		if (j <= 0 && j >= -100){
+			System.out.print(" ainsi que compris entre 0 et -100");
+		}
+		
+		else if (j <= -100){
+			System.out.print(" ainsi qu'inférieur a -100");
 		}
 		
 		}
