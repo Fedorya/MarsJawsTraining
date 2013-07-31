938f5eee2de43b430c780294fa6a5da546bcdb1d
WiwiPoupsi <veritas75100@gmail.com>
2013-07-31 10:05:14 +0200

    rev bacversion

--- Ben1/src/Debut1.java
+++ Ben1/src/Debut1.java
@@ -6,10 +6,26 @@ public class Debut1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //Chalut comment ça va ?
-		Scanner sc = new Scanner (System.in);
-		System.out.println("Veuillez saisir un mot : ");
-		String str = sc.nextLine();
-		System.out.println("Vous avez écris : " + str);
+		Scanner sc =  new Scanner(System.in);
+		System.out.println("Quelle est votre moyenne ? ");
+		int a = sc.nextInt();
+		switch (a)
+		{
+		case 0:
+			System.out.println("Petit ! tu n'auras jamais ton bac ! ");
+			break;
+		case 10:
+			System.out.println("Peut être que tu peux songer à avoir ton brevet, peut être... ");
+			break;
+		case 20:
+			System.out.println("Impossible ! ");
+			break;
+			default:
+				System.out.println("Petit ! Vous finirez sdf sous les ponts ! ");
+		}
+		
+		
+		
 		
 	}
 
