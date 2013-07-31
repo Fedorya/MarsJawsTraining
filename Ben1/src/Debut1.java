ff5ce2840676c5e7954034c73e6c4614bac58570
Fedorya <seb_diouf@hotmail.com>
2013-07-31 10:42:17 +0200

    If else

--- Ben1/src/Debut1.java
+++ Ben1/src/Debut1.java
@@ -5,7 +5,7 @@ public class Debut1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//Chalut comment ça va ?
+//Chalut comment è»‹ va ?
 		Scanner sc =  new Scanner(System.in);
 		System.out.println("Quelle est votre moyenne ? ");
 		int a = sc.nextInt();
@@ -15,13 +15,21 @@ public class Debut1 {
 			System.out.println("Petit ! tu n'auras jamais ton bac ! ");
 			break;
 		case 10:
-			System.out.println("Peut être que tu peux songer à avoir ton brevet, peut être... ");
+			System.out.println("Peut é»Žre que tu peux songer ï¿½avoir ton brevet, peut é»Žre... ");
 			break;
 		case 20:
 			System.out.println("Impossible ! ");
 			break;
 			default:
-				System.out.println("Petit ! Vous finirez sdf sous les ponts ! ");
+				if (a <10 && a >0)
+				{
+					System.out.println("C'est pas encore Ã§a !");
+				}
+				else 
+				{
+					System.out.println("Petit ! Vous finirez sdf sous les ponts ! ");
+				}
+				
 		}
 		
 		
