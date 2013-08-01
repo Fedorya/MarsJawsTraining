f675f9ea7d55cde553baba6b7ced61b0821016fc
WiwiPoupsi <veritas75100@gmail.com>
2013-08-01 14:58:17 +0200

    Help

--- Ben1/src/Test1.java
+++ Ben1/src/Test1.java
@@ -1,14 +1,57 @@
-
+import java.util.Scanner;
 public class Test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		for (long nbre1 = 1, nbre2 = 5; nbre1 <= 20 && nbre2 <= 100; nbre1++, nbre2+=5)
-		{
-		System.out.println("nbre1 =" + nbre1 + ",nbre2 =" + nbre2);
-		}
-
-
+		Scanner sc = new Scanner(System.in);
+		char reponse = ' ', choix = ' ';
+		
+		
+				System.out.println(" CONVERTISSEUROTRON 9000 DE DEGRÉS FAHRENHEIT/CELSIUS");
+				System.out.println("------------------------------------------------------");
+				
+			do{ 
+			do{
+			
+				System.out.println("Veuillez choisir le mode de conversion : ");
+				System.out.println("1 - Conversion degrés Celsius -> Fahrenheit :");
+				System.out.println("2 - Conversion degrés Fahrenheit -> Celsius :");
+				
+			choix = sc.next() .charAt(0);
+			
+			if (choix != '1' && choix != '2');
+				System.out.println("Choix inconnu, veuillez saisir un choix correct. ");
+				
+			}while (choix == 1 && choix == 2);
+				
+			if (choix == 1) {
+				System.out.println("(Celsius -> Fahrenheit) Température à convertir : ");
+				sc.nextLine();
+			float c = sc.nextFloat();
+			float f = (float) (9.0/5.0*c+32);
+				System.out.println(" " +c+ " °C correspond à : " +f+ " °F. ");
+			}
+				
+			
+			else {
+				
+			float f1 = sc.nextFloat();
+			float c = (float) (f1-32*5/9);
+				System.out.println(" " +f1+ " °F correspond à : " +c+ " °C.");
+			}
+			
+			
+			do {
+				System.out.println("Voulez-vous convertir une autre température O/N ? ");
+			reponse = sc.next() .charAt(0);
+			
+			}while (reponse != 'O' && reponse != 'N');
+			}while (reponse == 'O');
+		
+				System.out.println("Bye bye !");
+			
+	    
+	}
+		
 		
 	}
-}
